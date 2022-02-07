583e997e985c201bef6e8266552f9bee54fe15fc
networkdavit <lonecreation1612@gmail.com>
2022-02-07 17:25:39 +0400

    Adding an activity for home page and forgot password page

--- app/src/main/java/com/example/myandroidapp/MainActivity.java
+++ app/src/main/java/com/example/myandroidapp/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.myandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,14 +22,31 @@ public class MainActivity extends AppCompatActivity {
 
         Button loginbtn = (Button) findViewById(R.id.loginbtn);
 
+        TextView forgotpass = (TextView) findViewById(R.id.forgotpass);
+
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
                     Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                    openHome();
                 }else
                     Toast.makeText(MainActivity.this, "LOGIN FAILED!!!", Toast.LENGTH_SHORT).show();
             }
         });
+
+        forgotpass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHome();
+
+            }
+        });
+
+    }
+
+    public void openHome() {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
     }
 }
