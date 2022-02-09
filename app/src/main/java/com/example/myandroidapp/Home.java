1d0dadd90111990650274d983f4d48e6f203e7d2
networkdavit <lonecreation1612@gmail.com>
2022-02-09 14:34:38 +0400

    Adding icons

--- app/src/main/java/com/example/myandroidapp/Home.java
+++ app/src/main/java/com/example/myandroidapp/Home.java
@@ -3,6 +3,9 @@ package com.example.myandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ImageView phishing = (ImageView) findViewById(R.id.phishing);
+
+        phishing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Home.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
